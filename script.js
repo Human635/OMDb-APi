6119f4b67d88d4e40885e7cd87f4faaa13eea435
Xojiakbar <139034828+Human635@users.noreply.github.com>
2023-08-23 19:43:18 +0500

    баг2

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function loadMovieDetails(){
         movie.addEventListener('click', async () => {
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=1db30209`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=1db30209`);
             const movieDetails = await result.json();
             displayMovieDetails(movieDetails);
         });
